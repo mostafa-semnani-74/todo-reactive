8df36dcd0a06041846da7202af2cceb2da606b3b
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2024-06-29 17:13:20 +0330

    init todo reactive

---
+++ src/main/java/ir/mostafa/semnani/todo_reactive/TodoReactiveApplication.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.todo_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/mostafa/semnani/todo_reactive/TodoReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.todo_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
